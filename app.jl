55007077882f5057a1d4892769f4e9b2940d7a15
Nate Wienert <nwienert@zappos.com>
2013-01-14 14:16:10 -0800

    i'm feeling fat, and sassy

---
+++ app.jl
@@ -0,0 +1,67 @@
+module App
+
+  export model,
+         controller,
+         routes,
+         run
+
+  global _models = Dict(),
+         _controllers = Dict(),
+         _routes = Dict()
+
+  function model(methods, name::String)
+    println("Adding model $name")
+    _models[name] = methods
+  end
+
+  function controller(actions, name::String)
+    println("Adding controller $name, $actions")
+    _controllers[name] = actions
+  end
+
+  function routes(app_routes)
+    # Flip routes around
+    for route in app_routes
+      controller, path = route
+      _routes[path] = controller
+    end
+  end
+
+  function run()
+    # Main loop of web app
+    # For now just to test, lets just call posts#index
+    # run_route(routes["/"])
+  end
+
+  function run_route(path::String)
+
+  end
+
+end
+
+# using App
+
+### Routes
+App.routes({
+  # "post"           => [:create, :edit, :update, :destroy],
+  # "post"           => :resource,
+  "main#index"     => "/",
+  "posts#index"    => "/posts"
+})
+
+### Models
+App.model("Post") do
+  # something
+  function title()
+    "Title of post"
+  end
+end
+
+### Controllers
+App.controller("Posts") do
+  function index()
+    Post.title()
+  end
+end
+
+App.run()
